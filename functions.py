68abd5982c1d2e4f703d3a78fb551893ab16c3b9
AliYagizDizdaroglu <sotkaliyagiz@gmail.com>
2022-05-28 01:45:28 +0300

    28.05.2022

--- functions.py
+++ functions.py
@@ -6,6 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from sklearn import tree
 from sklearn import metrics
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
 
 def random_forest_regressor (X, y):
     train_X, val_X, train_y, val_y = train_test_split(X, y,random_state = 1)
@@ -51,4 +53,43 @@ def decision_tree_regressor(X, y):
     total_mean_error = (mae * 100) / y.mean()
     print(f"DT Total mean error: {total_mean_error:.4f} %")
     return(mae, prediction)
+
+def corr_analysis(df):
+    print("df.corr(method ='pearson').values: \n {} \n".format(df.corr(method ='pearson').values))
+    df2 = pd.DataFrame(df.corr(method ='pearson').values)
+    df2.columns = df.columns
+    df2.index = df.columns
+    df2 = df2.replace(1,0)
+    print("columns and rows added, 1's changed to 0's: \n {} \n".format(df2))
+
+    dictOfTuples = {}
+    for column in df2.columns:
+        for index in df2.index:
+            dictOfTuples[(column,index)] = df2.loc[column, index]
+    dictOfTuples = dict(sorted(dictOfTuples.items(), key=lambda item: item[1]))
+    dictOfTuples = {(column, index): value for (column, index), value in dictOfTuples.items() if column != index}
+    print("dictionary equivalent of the dataframe:(unique and sorted) \n {} \n".format(dictOfTuples))
+
+    keys = [str(key) for key in dictOfTuples.keys()]
+    keys = [key for key in keys if keys.index(key) % 2 == 0]
+    values = [value for value in dictOfTuples.values()]
+    values = sorted(set([value for value in values]))
+    #[value for value in values if values.index(value) % 2 == 0] does not work?
+
+    print("keys of the dictionary: \n {} \n".format(keys))
+    print("values of the dictionary: \n {} \n".format(values))
+
+    plot = plt.bar(keys,values)
+    plot = plt.xticks(rotation=90)
+    plot = plt.ylabel('pearson corr. value')
+    plot = plt.xlabel('column x column')
+    plt.show()
+
+    maxCorrVal = max(df2.max(axis=1))
+    print('max corr. value: \n {} \n'.format(maxCorrVal))
+    maxCorrKey = max(dictOfTuples, key=dictOfTuples.get)
+    print('columns/rows of max corr. value: \n {} \n'.format(maxCorrKey))
+
+    return dictOfTuples
+
     
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+import seaborn as sns
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
-from functions import random_forest_regressor, decision_tree_regressor
+from xgboost import XGBRegressor
+from functions import corr_analysis, random_forest_regressor, decision_tree_regressor
+import matplotlib.pyplot as plt
 
 
 # Path of the file to read
@@ -16,9 +19,22 @@ y = home_data.SalePrice
 # Create X
 features = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 X = home_data[features]
-
+df_pairplot = home_data[['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd', 'SalePrice']]
 
 maeRF, predictionRF = random_forest_regressor(X,y)
 decision_tree_regressor(X,y)
+print(home_data)
+dictOfTuples = dict(corr_analysis(df_pairplot))
 
-home_data.to_csv(predictionRF, encoding='utf-8')
+Y_dict = {(x,y):value for (x,y), value in dictOfTuples.items() if 'SalePrice' in (x,y)}
+
+keys = [str(key) for key in Y_dict.keys()]
+keys = [key for key in keys if keys.index(key) % 2 == 0]
+values = [value for value in Y_dict.values()]
+values = sorted(set([value for value in values]))
+
+plot = plt.bar(keys,values)
+plot = plt.xticks(rotation=90)
+plot = plt.ylabel('pearson corr. value')
+plot = plt.xlabel('column x column')
+plt.show()
